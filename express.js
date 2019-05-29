b7706f6cdebc116e88e830d0a9551b7d63789bff
Felipe <felipeperozo@hotmail.com>
2019-05-29 15:42:38 -0300

    commit

--- express.js
+++ express.js
@@ -1,18 +1,25 @@
+/*
+Crie um programa de agendamento de consultas onde o paciente poderá escolher data e hora para 
+um agendamento.Crie uma seção para o Medico acompanhar os agendamentos do dia programados com ele. 
+Essa seção deve incluir as informações do usuário que agendou e também o horário do agendamento.
 
-var express = require('express');
+
+*/
+let agendamentos = require("./clinics");
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-  });
-
-app.get('/doug', function (req, res) {
-    res.send('Eaemen!');
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
 
-app.get('/batata', function (req, res) {
-    res.send('!!!!!');
+app.get("/doug", function(req, res) {
+  res.send(agendamentos);
 });
-  app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.get("/batata", function(req, res) {
+  res.send("!!!!!");
+});
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
