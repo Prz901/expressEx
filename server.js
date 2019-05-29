ebb384ad001763e209b0a2c436d4326767f51cc6
Felipe <felipeperozo@hotmail.com>
2019-05-29 17:26:49 -0300

    commit html engine

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const app = require("./src/custom-express");
 const http = require("http");
 
 const engine = require("consolidate");
+const bodyParse = require("body-parser");
 
 app.set("views", __dirname + "/views"); // general config
 app.engine("html", engine.mustache);
 app.set("view engine", "html");
 
-//app.use()
+app.use(
+  bodyParse.urlencoded({
+    extended: true
+  })
+);
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
 });
